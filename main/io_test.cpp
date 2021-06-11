c269dad53c1c50f85e3cef2b2830e87c91a101fe
rnascunha85 <rnascunha@gmail.com>
2021-06-10 21:57:44 -0300

    Test water level

--- main/io_test.cpp
+++ main/io_test.cpp
@@ -28,67 +28,41 @@ extern "C" void app_main(void)
 {
 	ESP_LOGI(TAG, "Iniciando!");
 
-//	GPIO_Basic gpio_sensor(DS18B20_DATA);
-//	OneWire onewire(&gpio_sensor);
-//	Dallas_Temperature temp_sensor(&onewire);
-//
-//	GPIO_Basic led(ONBOARD_LED), out1(TRIAC_OUTPUT1);
-//	led.mode(GPIO_MODE_OUTPUT);
-//	out1.mode(GPIO_MODE_OUTPUT);
-//
-//	GPIO_Basic wl4(WATER_LEVEL4);
-//	wl4.mode(GPIO_MODE_INPUT);
-//
-//	printf("Water level: %d\n", wl4.read());
-//
-//	led.write(0);
-//	out1.write(0);
-//
-//	I2C_Master i2c(I2C_NUM_0, I2C_SCL, I2C_SDA, I2C_FAST_SPEED_HZ);
-//	i2c.init();
-//
-//	DS3231 rtc(&i2c);
-//	rtc.begin();
-//
-//	DateTime dt;
-//	//Hard code time
-//	dt.setUnixTime(1623109458);
-//	rtc.setDateTime(&dt);
+	GPIO_Basic gpio_sensor(DS18B20_DATA);
+	OneWire onewire(&gpio_sensor);
+	Dallas_Temperature temp_sensor(&onewire);
 
-	esp_err_t r;
-	adc2_config_channel_atten(WATER_LEVEL_ADC4, ADC_ATTEN_11db);
+	GPIO_Basic led(ONBOARD_LED, GPIO_MODE_OUTPUT),
+			out1(TRIAC_OUTPUT1, GPIO_MODE_OUTPUT);
+	GPIO_Basic wl4(WATER_LEVEL4, GPIO_MODE_INPUT);
 
-	gpio_num_t adc_gpio_num;
+	led.write(0);
+	out1.write(0);
 
-    r = adc2_pad_get_io_num( WATER_LEVEL_ADC4, &adc_gpio_num );
-	assert( r == ESP_OK );
+	I2C_Master i2c(I2C_NUM_0, I2C_SCL, I2C_SDA, I2C_FAST_SPEED_HZ);
+	i2c.init();
 
-	printf("ADC2 channel %d @ GPIO %d\n", WATER_LEVEL_ADC4, adc_gpio_num);
+	DS3231 rtc(&i2c);
+	rtc.begin();
 
-	vTaskDelay(2 * portTICK_PERIOD_MS);
-	int     read_raw;
+	DateTime dt;
+	//Hard code time
+	dt.setUnixTime(1623109458);
+	rtc.setDateTime(&dt);
 
 	while(true){
-//		temp_sensor.requestTemperatures();
-//		led.toggle();
-//		out1.toggle();
-//		rtc.getDateTime(&dt);
+		temp_sensor.requestTemperatures();
+		led.toggle();
+		out1.toggle();
+		rtc.getDateTime(&dt);
 
-		r = adc2_get_raw( WATER_LEVEL_ADC4, width, &read_raw);
-		if ( r == ESP_OK ) {
-			printf("%d\n", read_raw );
-		}
-		else {
-			printf("%s\n", esp_err_to_name(r));
-		}
-
-//		printf("Time: %u/%u/%u %u:%u:%u %u | Temp(C): %f | WL4: %d\n",
-//				dt.getDay(), dt.getMonth(), dt.getYear(),
-//				dt.getHour(), dt.getMinute(), dt.getSecond(),
-//				dt.dayOfWeek(),
-//				temp_sensor.getTempCByIndex(0),
-//				wl4.read()
-//				);
+		printf("Time: %u/%u/%u %u:%u:%u %u | Temp(C): %f | WL4: %d\n",
+				dt.getDay(), dt.getMonth(), dt.getYear(),
+				dt.getHour(), dt.getMinute(), dt.getSecond(),
+				dt.dayOfWeek(),
+				temp_sensor.getTempCByIndex(0),
+				wl4.read()
+				);
 
 		vTaskDelay(1000 / portTICK_PERIOD_MS);
 	}
